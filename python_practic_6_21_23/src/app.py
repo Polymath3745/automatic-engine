c7131c3ea72b7be8297aa5d5a4818f0a2d03c0cb
Gabriel <gabriel@gabriel-System-Product-Name>
2023-06-21 16:11:29 -0700

    quiz program

--- python_practic_6_21_23/src/app.py
+++ python_practic_6_21_23/src/app.py
@@ -1,7 +1,23 @@
-# from file import this class
-from student import Student
+from question import Question
 
-student1 = Student("Jim", "Business", 3.1, False)
+questions_prompts = [
+    "What color is an apple?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color is a banana?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color is a strawberry?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
 
-student1.disp_name()
-print(student1.m_gpa)
+questions = [
+    Question(questions_prompts[0], "a"),
+    Question(questions_prompts[1], "c"),
+    Question(questions_prompts[2], "b"),
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.m_prompt)
+        if answer == question.m_answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(questions)
---
+++ python_practic_6_21_23/src/question.py
@@ -0,0 +1,4 @@
+class Question:
+    def __init__(self, prompt, answer):
+        self.m_prompt = prompt
+        self.m_answer = answer
