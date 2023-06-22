c7e7e6aebcf55b9e288dda7250686c115f465dab
Gabriel <gabriel@gabriel-System-Product-Name>
2023-06-22 16:00:03 -0700

    inheritance practice

--- python_practice_6_20_23/src/app.py
+++
@@ -1,7 +0,0 @@
-# from file import this class
-from student import Student
-
-student1 = Student("Jim", "Business", 3.1, False)
-
-print(student1.m_name)
-print(student1.m_gpa)
--- python_practice_6_22_23/src/app.py
+++ python_practice_6_22_23/src/app.py
@@ -1,6 +1,7 @@
 from question import Question
 from student import Student
-
+from chef import Chef
+from chineseChef import ChineseChef
 questions_prompts = [
     "What color is an apple?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
     "\nWhat color is a banana?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
@@ -16,7 +17,8 @@ questions = [
 ]
 
 student1 = Student("Gabriel", "Computer Engineering", 3.4, False)
-
+chef = Chef()
+chinese_chef = ChineseChef()
 
 def run_test(questions):
     score = 0
@@ -29,7 +31,11 @@ def run_test(questions):
 
 def main():
     #run_test(questions)
-    print(student1.on_honor_roll())
+    #print(student1.on_honor_roll())
+    chef.make_chicken()
+    chef.make_salad()
+    chef.make_special_dish()
+    chinese_chef.make_special_dish()
 
 
 if __name__ == "__main__":
