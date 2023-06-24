14c54cc2ab45c5dc1014369755cfa2e1e784edeb
Gabriel <gabriel@gabriel-System-Product-Name>
2023-06-24 15:45:08 -0700

    did something

--- python_practice_6_24_23/src/app.py
+++ python_practice_6_24_23/src/app.py
@@ -5,8 +5,6 @@ def main():
     soldier2 = Soldier("Omid", 23455, 23003, {"private" : 1})
 
     soldier1.seniority(soldier2)
-    #soldier1_rank_value = list(soldier1.m_rank.values())[0]
-    #print(type(soldier1_rank_value))
 
 if __name__ == "__main__":
     main()
