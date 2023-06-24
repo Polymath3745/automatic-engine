307728758aff0abcc0b9c77d94db42fc614643c3
Gabriel <gabriel@gabriel-System-Product-Name>
2023-06-24 15:35:31 -0700

    refined the rank comparison function

--- python_practice_6_24_23/src/soldier.py
+++ python_practice_6_24_23/src/soldier.py
@@ -34,9 +34,16 @@ class Soldier:
     def set_rank(self, rank):
         self.m_rank = rank
 
+    # This function takes in the rank of the calling soldier and the comparison one
+    # it then uses the values dict function to grab the rank value
+    # passes this to the list function which then grabs it as a integer and compares
+    # to the rank value of the other soldier
+
     def seniority(self, other_soldier):
-        if(self.m_rank.keys() < other_soldier.m_rank.keys()):
+        if(list(self.m_rank.values())[0] > list(other_soldier.m_rank.values())[0]):
             print(self.m_name + " is a higher rank than " + other_soldier.m_name)
+            print(self.m_name + " is a " + list(self.m_rank.keys())[0])
+            print(other_soldier.m_name + " is a " + list(other_soldier.m_rank.keys())[0])
 
         else:
-            print(self.m_name + " is a higher rank than " + other_soldier.m_name)
+            print(other_soldier.m_name + " is a higher rank than " + self.m_name)
