03cd9ee96a5494c82857ffa1b4198c542657c37e
Gabriel <gabriel@gabriel-System-Product-Name>
2023-06-24 15:05:05 -0700

    finished making setters, created rank comparison function for soldiers

---
+++ python_practice_6_24_23/src/app.py
@@ -0,0 +1,10 @@
+from soldier import Soldier
+
+def main():
+    soldier1 = Soldier("Gabriel J.", 1234, 1301998, {"captain" : 5})
+    soldier2 = Soldier("Omid", 23455, 23003, {"private" : 1})
+
+    soldier1.seniority(soldier2)
+
+if __name__ == "__main__":
+    main()
--- python_practice_6_24_23/src/soldier.py
+++ python_practice_6_24_23/src/soldier.py
@@ -6,6 +6,8 @@ class Soldier:
         self.m_birthday = birthday
         self.m_rank = rank
 
+    
+    # getters
     def get_name(self):
         return self.m_name
     
@@ -18,3 +20,23 @@ class Soldier:
     def get_rank(self):
         return self.m_rank
     
+
+    # setters
+    def set_name(self, name):
+        self.m_name = name
+
+    def set_id(self, id):
+        self.m_id = id
+
+    def set_birthday(self, birthday):
+        self.m_birthday = birthday
+
+    def set_rank(self, rank):
+        self.m_rank = rank
+
+    def seniority(self, other_soldier):
+        if(self.m_rank.keys() < other_soldier.m_rank.keys()):
+            print(self.m_name + " is a higher rank than " + other_soldier.m_name)
+
+        else:
+            print(self.m_name + " is a higher rank than " + other_soldier.m_name)
